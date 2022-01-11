98c8281a1a5dfe48a92a71d1438f4249339f0d57
조 재 호 <86958131+HOJAE98@users.noreply.github.com>
2022-01-12 04:01:53 +0900

    [CHORE] camelCase로 수정

--- functions/db/request.js
+++ functions/db/request.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 
-const postInsertRequestLecture = async (client, categoryid ,skill, email) => {
+const postInsertRequestLecture = async (client, categoryId ,skill, email) => {
     const { rows } = await client.query(
         `INSERT INTO request
         (category_id, skill, email)
@@ -10,7 +10,7 @@ const postInsertRequestLecture = async (client, categoryid ,skill, email) => {
         ($1,$2,$3)
         RETURNING *
         `,
-        [categoryid, skill, email],
+        [categoryId, skill, email],
     );
     return convertSnakeToCamel.keysToCamel(rows);
 };
