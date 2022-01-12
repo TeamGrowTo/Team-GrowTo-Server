a52fcfdcb322e2c2e190095b184177398152e9d9
조 재 호 <86958131+HOJAE98@users.noreply.github.com>
2022-01-12 20:52:14 +0900

    [UPDATE] 쿼리 코드 수정

--- functions/db/lectures.js
+++ functions/db/lectures.js
@@ -6,7 +6,7 @@ const getRankRequestLecture = async (client) => {
         `SELECT c.name,skill, COUNT(skill) AS number FROM request
             LEFT JOIN category c on c.id = request.category_id
          GROUP BY  c.name,skill
-         HAVING COUNT(skill) > 0
+         HAVING COUNT(skill) < 100
          ORDER BY number DESC
          LIMIT 8;
         `,
@@ -15,21 +15,11 @@ const getRankRequestLecture = async (client) => {
 };
 
 
-const getComparingRequestLecture= async(client) => {
-    const { rows } = await client.query(
-        `SELECT c.name,skill AS number FROM request
-            LEFT JOIN category c on c.id = request.category_id
-        GROUP BY  c.name,skill
-        HAVING COUNT(skill) >=3
-        ORDER BY number DESC
-        LIMIT 8;
-        `,
-       
-    );
-    return convertSnakeToCamel.keysToCamel(rows);
-};
 
-module.exports = {getRankRequestLecture, getComparingRequestLecture };
+
+module.exports = {
+    getRankRequestLecture, 
+   };
 
 
 
