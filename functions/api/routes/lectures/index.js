b231c0c0bcf64b1901c8b294b07c3cfdb948fcc0
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-12 15:58:32 +0900

    [UPDATE] URI경로 수정

--- functions/api/routes/lectures/index.js
+++ functions/api/routes/lectures/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/lectures', require('./lectures'));
+router.post('/request', require('./requestLecturePOST'));
 
 module.exports = router;
