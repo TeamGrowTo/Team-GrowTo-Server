5b5016cff0e219cc9f3f6685ed4fb63f8d0ff80a
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-12 17:56:37 +0900

    [FEAT] 주간 인기순위 조회 로직 추가

--- functions/db/lecture.js
+++ functions/db/lecture.js
@@ -1,8 +1,23 @@
 const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
+const getRankRequestLecture = async (client) => {
+    const { rows } = await client.query(
+        `SELECT c.name,skill, COUNT(skill) AS number FROM request
+            LEFT JOIN category c on c.id = request.category_id
+         GROUP BY  c.name,skill
+         HAVING COUNT(skill) > 0
+         ORDER BY number DESC
+         LIMIT 8;
+        `,
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+};
 
 
 
 
-module.exports = { };
+module.exports = {getRankRequestLecture, };
+
+
+
