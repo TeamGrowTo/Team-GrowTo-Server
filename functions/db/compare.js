62952651a9359f19df8909006b810a64cbae72f7
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-14 03:33:30 +0900

    [FEAT] 현재 비교중인 강의API구현

--- functions/db/compare.js
+++ functions/db/compare.js
@@ -1,3 +1,19 @@
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
-module.exports = {};
+const getComparingLecture = async (client) => {
+    const { rows } = await client.query(
+        `
+        SELECT c.name,skill FROM request
+        LEFT JOIN category c on c.id = request.category_id
+        GROUP BY  c.name,skill
+        HAVING COUNT(skill) >=100
+        ORDER BY COUNT(skill) DESC
+        LIMIT 6;
+      `,
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { getComparingLecture };
+
+
