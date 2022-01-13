60172d0a412169cd01040c06da4902c448fa34e2
NownS <kimjuicy29@gmail.com>
2022-01-13 23:48:10 +0900

    [update] : "responseMessage.js" 분야 조회 성공 메시지 추가

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -1,22 +1,25 @@
 module.exports = {
-    NULL_VALUE: '필요한 값이 없습니다',
-    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
-  
-    // 회원가입
-    CREATED_USER: '회원 가입 성공',
-    DELETE_USER: '회원 탈퇴 성공',
-    ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
-  
-    // 로그인
-    LOGIN_SUCCESS: '로그인 성공',
-    LOGIN_FAIL: '로그인 실패',
-    NO_USER: '존재하지 않는 회원입니다.',
-    MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
-  
-    // 프로필 조회
-    READ_PROFILE_SUCCESS: '프로필 조회 성공',
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
-    //비교요청 성공 
-    COMPARE_REQUEST_SUCCESS: '강의 비교요청 성공',
-    COMPARE_REQUEST_RANK_SUCCESS: '비교요청 인기순위 불러오기 성공',
-  };
+  // 회원가입
+  CREATED_USER: '회원 가입 성공',
+  DELETE_USER: '회원 탈퇴 성공',
+  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+
+  // 로그인
+  LOGIN_SUCCESS: '로그인 성공',
+  LOGIN_FAIL: '로그인 실패',
+  NO_USER: '존재하지 않는 회원입니다.',
+  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+
+  // 프로필 조회
+  READ_PROFILE_SUCCESS: '프로필 조회 성공',
+
+  //비교요청 성공 
+  COMPARE_REQUEST_SUCCESS: '강의 비교요청 성공',
+  COMPARE_REQUEST_RANK_SUCCESS: '비교요청 인기순위 불러오기 성공',
+
+  //정보 확인 성공 
+  READ_CATEGORIES_SUCCESS: '분야 조회 성공',
+};
