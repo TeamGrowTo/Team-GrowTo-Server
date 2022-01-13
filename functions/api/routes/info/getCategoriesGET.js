60a95479dc1a1ac8fb45d730a6bb54fcc5b169d3
yoon <nicealice99@gmail.com>
2022-01-14 02:11:57 +0900

    [CHORE] prettier 적용

--- functions/api/routes/info/getCategoriesGET.js
+++ functions/api/routes/info/getCategoriesGET.js
@@ -6,16 +6,16 @@ const db = require('../../../db/db');
 const { categoryDB } = require('../../../db');
 
 module.exports = async (req, res) => {
-    let client;
-    try {
-        client = await db.connect(req);
-        const categories = await categoryDB.getAllCategories(client);
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CATEGORIES_SUCCESS, categories));
-    } catch (error) {
-        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-        client.release();
-    }
+  let client;
+  try {
+    client = await db.connect(req);
+    const categories = await categoryDB.getAllCategories(client);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_CATEGORIES_SUCCESS, categories));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
 };
