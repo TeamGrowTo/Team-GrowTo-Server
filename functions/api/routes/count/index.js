56aa3d9b604f9cce75fdd7e6ff56c8957103847f
yoon <nicealice99@gmail.com>
2022-01-14 03:48:26 +0900

    [FEAT] 강의찾기횟수 조회하기 구현

--- functions/api/routes/count/index.js
+++ functions/api/routes/count/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/total', require('./totalLectureGET'));
+router.get('/find', require('./findLectureGET'));
 
 module.exports = router;
--- functions/db/find.js
+++ functions/db/find.js
@@ -1,3 +1,10 @@
-const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+const getFindTotal = async (client) => {
+  const { rowCount: findNumber } = await client.query(
+    `
+    SELECT * FROM "find"
+    `,
+  );
+  return { findNumber };
+};
 
-module.exports = {};
+module.exports = { getFindTotal };
