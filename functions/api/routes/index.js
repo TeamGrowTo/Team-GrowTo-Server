11d40acd4882e6fd7e126c44c2e239d89caf3b8a
NownS <kimjuicy29@gmail.com>
2022-01-13 23:45:38 +0900

    [update] : "index.js" info 라우팅 추가

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/lectures', require('./lectures'));
+router.use('/info', require('./info'));
 
 module.exports = router;
