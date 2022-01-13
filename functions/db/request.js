d74e59c986384695d7c7f4af6256eec8a7b05f6c
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-14 03:22:00 +0900

    [CHORE] 불필요한 코드삭제

--- functions/db/request.js
+++ functions/db/request.js
@@ -13,17 +13,4 @@ const postInsertRequestLecture = async (client, categoryId, skill, email) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-const getRankRequestLecture = async (client) => {
-  const { rows } = await client.query(
-    `SELECT c.name,skill, COUNT(skill) AS number FROM request
-    LEFT JOIN category c on c.id = request.category_id
-    GROUP BY  c.name,skill
-    HAVING COUNT(skill) < 100
-    ORDER BY number DESC
-    LIMIT 8;
-    `,
-  );
-  return convertSnakeToCamel.keysToCamel(rows);
-};
-
 module.exports = { postInsertRequestLecture };
