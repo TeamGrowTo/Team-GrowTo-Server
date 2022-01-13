ad65b86daaa4d5f5740b269a74e8282814b3f06e
yoon <nicealice99@gmail.com>
2022-01-14 02:05:32 +0900

    [UPDATE] 비교요청 인기순위 조회 수정

--- functions/api/routes/lectures/lectureRankGET.js
+++ functions/api/routes/lectures/lectureRankGET.js
@@ -3,19 +3,19 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { lectureDB } = require('../../../db');
+const { requestDB } = require('../../../db');
 
 module.exports = async (req, res) => {
-    let client;
-    try {
-        client = await db.connect(req);
-        const rankrequestlecture = await lectureDB.getRankRequestLecture(client);
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.COMPARE_REQUEST_RANK_SUCCESS, rankrequestlecture));
-    } catch (error) {
-        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-        client.release();
-    }
+  let client;
+  try {
+    client = await db.connect(req);
+    const rankrequestlecture = await requestDB.getRankRequestLecture(client);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_REQUEST_RANK_SUCCESS, rankrequestlecture));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
 };
--- functions/db/request.js
+++ functions/db/request.js
@@ -1,20 +1,29 @@
-const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
-
-const postInsertRequestLecture = async (client, categoryId ,skill, email) => {
-    const { rows } = await client.query(
-        `INSERT INTO request
-        (category_id, skill, email)
-        VALUES
-        ($1,$2,$3)
-        RETURNING *
-        `,
-        [categoryId, skill, email],
-    );
-    return convertSnakeToCamel.keysToCamel(rows);
+const postInsertRequestLecture = async (client, categoryId, skill, email) => {
+  const { rows } = await client.query(
+    `INSERT INTO request
+    (category_id, skill, email)
+    VALUES
+    ($1,$2,$3)
+    RETURNING *
+    `,
+    [categoryId, skill, email],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
 };
 
+const getRankRequestLecture = async (client) => {
+  const { rows } = await client.query(
+    `SELECT c.name,skill, COUNT(skill) AS number FROM request
+    LEFT JOIN category c on c.id = request.category_id
+    GROUP BY  c.name,skill
+    HAVING COUNT(skill) < 100
+    ORDER BY number DESC
+    LIMIT 8;
+    `,
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
 
-
-module.exports = { postInsertRequestLecture};
+module.exports = { postInsertRequestLecture, getRankRequestLecture };
