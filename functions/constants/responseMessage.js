df56cdb23de470119a042cd0a35216987f7c26a0
yoon <nicealice99@gmail.com>
2022-01-13 22:08:47 +0900

    [FEAT] 강의 개수 관련 responseMessage 추가

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -1,21 +1,21 @@
 module.exports = {
-    NULL_VALUE: '필요한 값이 없습니다',
-    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
-  
-    // 회원가입
-    CREATED_USER: '회원 가입 성공',
-    DELETE_USER: '회원 탈퇴 성공',
-    ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
-  
-    // 로그인
-    LOGIN_SUCCESS: '로그인 성공',
-    LOGIN_FAIL: '로그인 실패',
-    NO_USER: '존재하지 않는 회원입니다.',
-    MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
-  
-    // 프로필 조회
-    READ_PROFILE_SUCCESS: '프로필 조회 성공',
+  NULL_VALUE: '필요한 값이 없습니다',
+  OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
 
-    //비교요청 성공 
-    COMPARE_REQUEST_SUCCESS: '강의 비교요청 성공'
-  };
+  // 회원가입
+  CREATED_USER: '회원 가입 성공',
+  DELETE_USER: '회원 탈퇴 성공',
+  ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+
+  // 로그인
+  LOGIN_SUCCESS: '로그인 성공',
+  LOGIN_FAIL: '로그인 실패',
+  NO_USER: '존재하지 않는 회원입니다.',
+  MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+
+  //비교요청 성공
+  COMPARE_REQUEST_SUCCESS: '강의 비교요청 성공',
+
+  // 총 개수
+  GET_LECTURE_TOTAL_SUCCESS: '총 강의개수 조회 성공',
+};
