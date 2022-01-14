1a684ddefc987b4cce6999972721f1174ddb8e0d
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-15 00:34:59 +0900

    [FEAT] Skill에맞는 tags불러오기 API

--- functions/db/tag.js
+++ functions/db/tag.js
@@ -1,3 +1,18 @@
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
-module.exports = {};
+const getSkillTags = async (client, skillId) => {
+    const { rows } = await client.query(
+        `
+      SELECT tag.id, tag.name FROM tag
+      LEFT JOIN skill s on s.id = tag.skill_id
+      WHERE skill_id = $1
+      ORDER BY tag.id
+      `,
+        [skillId],
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { getSkillTags };
+
+
