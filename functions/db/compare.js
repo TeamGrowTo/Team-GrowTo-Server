d3785f8915105f10553d6e309bf7653859b6a2e5
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-14 15:48:13 +0900

    [FEAT] CHORE 각테이블 조회하는방식으로 수정

--- functions/db/compare.js
+++ functions/db/compare.js
@@ -3,11 +3,8 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const getComparingLecture = async (client) => {
     const { rows } = await client.query(
         `
-        SELECT c.name,skill FROM request
-        LEFT JOIN category c on c.id = request.category_id
-        GROUP BY  c.name,skill
-        HAVING COUNT(skill) >=100
-        ORDER BY COUNT(skill) DESC
+        SELECT c.name , skill FROM comparing
+        LEFT JOIN category c on c.id = comparing.category_id
         LIMIT 6;
       `,
     );
