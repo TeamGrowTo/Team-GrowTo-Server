d9af2c59d3f50735d597dedce30f9fbb6faffa63
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-14 15:50:46 +0900

    [FEAT] CHORE 각테이블 조회하는방식으로 수정

--- functions/db/rank.js
+++ functions/db/rank.js
@@ -5,7 +5,8 @@ const getRankRequestLecture = async (client) => {
         `
         SELECT c.name , skill ,number FROM ranking
         LEFT JOIN category c on c.id = ranking.category_id
-       `,
+        LIMIT 8;
+        `,
     );
     return convertSnakeToCamel.keysToCamel(rows);
 };
