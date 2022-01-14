408017e2bad38e5c34b48ef889ae8687ff686646
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-15 00:34:20 +0900

    [FEAT] Tags라우팅 추가

--- functions/api/routes/info/index.js
+++ functions/api/routes/info/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/categories', require('./getCategoriesGET'));
+router.get('/:skillId/tags', require('./skillTagsGET'));
 
 module.exports = router;
