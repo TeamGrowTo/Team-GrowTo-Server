c205688db784ac9336d35fa9e3f967124a36651d
yoon <nicealice99@gmail.com>
2022-01-16 23:28:41 +0900

    [FEAT] 다른 정보 신고하기 구현

--- functions/api/routes/lectures/index.js
+++ functions/api/routes/lectures/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/request', require('./requestLecturePOST'));
+router.post('/report', require('./reportPOST'));
 router.get('/rank', require('./lectureRankGET'));
 
 module.exports = router;
---
+++ functions/api/routes/lectures/reportPOST.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { reportDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { reasonId, lecture, explanation, email } = req.body;
+
+  if (!reasonId || !lecture || !email) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
+
+  let client;
+  try {
+    client = await db.connect(req);
+    const report = await reportDB.addReport(client, reasonId, lecture, explanation, email);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_REPORT_SUCCESS, report));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
