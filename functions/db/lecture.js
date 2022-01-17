4eda95e2a5e8d44f6537e15551ce2dd124ccb2dc
NownS <kimjuicy29@gmail.com>
2022-01-18 01:58:01 +0900

    [UPDATE] 강의 가져오기 관련 쿼리 추가

--- functions/db/lecture.js
+++ functions/db/lecture.js
@@ -1,3 +1,5 @@
+const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+
 const getLectureTotal = async (client) => {
   const { rowCount: totalNumber } = await client.query(
     `
@@ -7,4 +9,35 @@ const getLectureTotal = async (client) => {
   return { totalNumber };
 };
 
-module.exports = { getLectureTotal };
+const getLectures = async (client, categoryId, skillId, order) => {
+  if (order.isDesc) {
+    const { rows } = await client.query(
+      `
+      SELECT l.id as id, l.name as name, price, time, start_year, duration, review_time, has_preview, has_optional, url, t.name as tag_name
+      FROM lecture as l, tag_lecture as tl, tag as t, review_time as rt
+      WHERE l.id = tl.lecture_id and tl.tag_id = t.id and l.review_time_id = rt.id
+      AND l.category_id = $1 and l.skill_id = $2
+      ORDER BY $3 DESC, l.name
+      `,
+      [categoryId, skillId, order.column]
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+  } else {
+    const { rows } = await client.query(
+      `
+      SELECT l.id as id, l.name as name, price, time, start_year, duration, review_time, has_preview, has_optional, url, t.name as tag_name
+      FROM lecture as l, tag_lecture as tl, tag as t, review_time as rt
+      WHERE l.id = tl.lecture_id and tl.tag_id = t.id and l.review_time_id = rt.id
+      AND l.category_id = $1 and l.skill_id = $2
+      ORDER BY $3, l.name
+      `,
+      [categoryId, skillId, order.column]
+    );
+    return convertSnakeToCamel.keysToCamel(rows);
+  }
+}
+
+module.exports = {
+  getLectureTotal,
+  getLectures
+};
