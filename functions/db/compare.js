fd48065f14eca64d4733cce8d742e11ee7d979b1
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-17 22:48:12 +0900

    [CHORE] 쿼리문 id순으로 정렬

--- functions/db/compare.js
+++ functions/db/compare.js
@@ -1,14 +1,15 @@
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const getComparingLecture = async (client) => {
-    const { rows } = await client.query(
-        `
+  const { rows } = await client.query(
+    `
         SELECT c.name , skill FROM comparing
         LEFT JOIN category c on c.id = comparing.category_id
+        ORDER BY comparing.id
         LIMIT 6;
       `,
-    );
-    return convertSnakeToCamel.keysToCamel(rows);
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
 };
 
 module.exports = { getComparingLecture };
