1f9f8700d34cbb9bffd00b83406ba7c42892bef3
NownS <kimjuicy29@gmail.com>
2022-01-18 03:19:40 +0900

    [CHORE] order by 적용 문제 해결

--- functions/db/lecture.js
+++ functions/db/lecture.js
@@ -1,4 +1,5 @@
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+const format = require('pg-format');
 
 const getLectureTotal = async (client) => {
   const { rowCount: totalNumber } = await client.query(
@@ -26,28 +27,28 @@ const getLectures = async (client, categoryId, skillId, order) => {
   }
 
   if (order.isDesc) {
-    const { rows } = await client.query(
+    const sql = format(
       `
       SELECT l.id as id, l.name as name, price, time, start_year, duration, review_time, has_preview, has_optional, url, t.name as tag_name
       FROM lecture as l, tag_lecture as tl, tag as t, review_time as rt
       WHERE l.id = tl.lecture_id and tl.tag_id = t.id and l.review_time_id = rt.id
-      AND l.category_id = $1 and l.skill_id = $2
-      ORDER BY $3 DESC, l.name
-      `,
-      [categoryId, skillId, order.column]
-    );
+      AND l.category_id = %s and l.skill_id = %s
+      ORDER BY %I DESC, l.name
+      `, categoryId, skillId, order.column
+    )
+    const { rows } = await client.query(sql);
     return convertSnakeToCamel.keysToCamel(rows);
   } else {
-    const { rows } = await client.query(
+    const sql = format(
       `
       SELECT l.id as id, l.name as name, price, time, start_year, duration, review_time, has_preview, has_optional, url, t.name as tag_name
       FROM lecture as l, tag_lecture as tl, tag as t, review_time as rt
       WHERE l.id = tl.lecture_id and tl.tag_id = t.id and l.review_time_id = rt.id
-      AND l.category_id = $1 and l.skill_id = $2
-      ORDER BY $3, l.name
-      `,
-      [categoryId, skillId, order.column]
-    );
+      AND l.category_id = %s and l.skill_id = %s
+      ORDER BY %I, l.name
+      `, categoryId, skillId, order.column
+    )
+    const { rows } = await client.query(sql);
     return convertSnakeToCamel.keysToCamel(rows);
   }
 }
