e9971fe75a7dbfbb4805b3da60eb21979d02a175
NownS <kimjuicy29@gmail.com>
2022-01-17 22:40:11 +0900

    [UPDATE] skill 데이터 형식 변경

--- functions/db/skill.js
+++ functions/db/skill.js
@@ -3,9 +3,10 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const getSkillsByCategoryId = async (client, categoryId) => {
     const { rows } = await client.query(
         `
-      SELECT *
+      SELECT id, name
       FROM skill
       WHERE category_id = $1
+      ORDER BY id
       `,
         [categoryId],
     );
