6463216b5c0583405df137b34fb64e7a55654e29
NownS <kimjuicy29@gmail.com>
2022-01-19 04:23:22 +0900

    [UPDATE] Response Message 추가

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -2,6 +2,7 @@ module.exports = {
   NULL_VALUE: '필요한 값이 없습니다',
   OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
   INTERNAL_SERVER_ERROR: "서버 내부 오류",
+  NO_CONTENTS: '조건을 만족하는 강의가 존재하지 않습니다',
 
   // 개수 조회
   READ_LECTURE_TOTAL_SUCCESS: '강의개수 조회 성공',
@@ -19,6 +20,7 @@ module.exports = {
   READ_TAGS_SUCCESS: '태그 조회 성공',
   READ_REASON_SUCCESS: '신고 사유 조회 성공',
   READ_NO_TAGS: '해당하는 스킬에 태그가 없습니다.',
+  READ_RECOMMEND_SUCCESS: '추천 강의 조회 성공',
 
   // 특정 분야, 스킬의 강의 목록 조회
   READ_LECTURES_SUCCESS: '강의 목록 조회 성공',
