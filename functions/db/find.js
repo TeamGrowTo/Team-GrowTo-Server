46e8b652dde571a3cc083eb495e00df3ad667391
NownS <kimjuicy29@gmail.com>
2022-01-19 04:24:33 +0900

    [UPDATE] find 테이블의 정보를 가져오기 위한 쿼리 추가

--- functions/db/find.js
+++ functions/db/find.js
@@ -1,3 +1,5 @@
+const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
+
 const getFindTotal = async (client) => {
   const { rowCount: findNumber } = await client.query(
     `
@@ -7,4 +9,18 @@ const getFindTotal = async (client) => {
   return { findNumber };
 };
 
-module.exports = { getFindTotal };
+const getSearchParams = async (client, findId) => {
+  const { rows } = await client.query(
+    `
+      SELECT * from "find"
+      WHERE id = $1
+    `,
+    [findId]
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+module.exports = {
+  getFindTotal,
+  getSearchParams,
+};
