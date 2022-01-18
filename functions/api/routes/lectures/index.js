e5f2c7ad6a1ec76d30d9622d9bb63a8eeed7d13b
NownS <kimjuicy29@gmail.com>
2022-01-19 04:23:04 +0900

    [UPDATE] /lectures/result/{find:id} 라우팅 추가

--- functions/api/routes/lectures/index.js
+++ functions/api/routes/lectures/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.post('/request', require('./requestLecturePOST'));
 router.get('/rank', require('./lectureRankGET'));
 router.get('/compare', require('./comparingLectureGET'));
+router.get('/result/:findId', require('./getResultGET'));
 
 module.exports = router;
