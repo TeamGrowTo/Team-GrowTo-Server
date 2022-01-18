d8ded1cb9bb3535206f1ac20fe30f9ca1f643f6d
NownS <kimjuicy29@gmail.com>
2022-01-19 03:36:59 +0900

    [CHORE] tag id가 아닌 name으로 저장하도록 변경

--- functions/db/find.js
+++ functions/db/find.js
@@ -33,33 +33,11 @@ const insertSearchLecture = async (client, category, skill, tags, timeAsc, price
 
   query = await client.query(
     `
-    SELECT id
-    FROM tag
-    where name = $1
-    `,
-    [tags[0]]
-  )
-  rows = query.rows
-  const tagId1 = rows[0].id;
-
-  query = await client.query(
-    `
-    SELECT id
-    FROM tag
-    where name = $1
-    `,
-    [tags[1]]
-  );
-  rows = query.rows
-  const tagId2 = rows[0].id;
-
-  query = await client.query(
-    `
-    INSERT INTO find(category_id, skill_id, tag_id, time_asc, price_asc)
+    INSERT INTO find(category_id, skill_id, tag_name, time_asc, price_asc)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING *
     `,
-    [categoryId, skillId, [tagId1, tagId2], timeAsc, priceAsc]
+    [categoryId, skillId, tags, timeAsc, priceAsc]
   );
   rows = query.rows
   return convertSnakeToCamel.keysToCamel(rows[0]);
