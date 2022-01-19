b014fff70e153659932bcf936ab4f05f4733db0b
NownS <kimjuicy29@gmail.com>
2022-01-20 00:18:33 +0900

    [CHORE] 라우팅 오류 해결

--- functions/api/routes/lectures/index.js
+++ functions/api/routes/lectures/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 router.post('/request', require('./requestLecturePOST'));
 router.post('/report', require('./reportPOST'));
 router.get('/rank', require('./lectureRankGET'));
-router.get('/:categoryId/:skillId', require('./getLecturesGET'));
 router.get('/compare', require('./comparingLectureGET'));
-router.get('/result/:findId', require('./getResultGET'));
 router.post('/search', require('./lectureSearchPOST'));
+router.get('/result/:findId', require('./getResultGET'));
+router.get('/:categoryId/:skillId', require('./getLecturesGET'));
 
 module.exports = router;
