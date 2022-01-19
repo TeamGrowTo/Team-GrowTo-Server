ff642800a2de09f3c5d2d841fc52c4bc36cdc356
yoon <nicealice99@gmail.com>
2022-01-20 00:26:03 +0900

    [FIX] eslint로 인한 while문 수정

--- functions/api/routes/lectures/getLecturesGET.js
+++ functions/api/routes/lectures/getLecturesGET.js
@@ -6,50 +6,50 @@ const db = require('../../../db/db');
 const { lectureDB, orderingDB } = require('../../../db');
 
 module.exports = async (req, res) => {
-    let client;
-    const { categoryId, skillId } = req.params;
-    let { ordering } = req.query;
+  let client;
+  const { categoryId, skillId } = req.params;
+  let { ordering } = req.query;
 
-    if (ordering === undefined) {
-        ordering = "default";
+  if (ordering === undefined) {
+    ordering = 'default';
+  }
+  ordering = ordering.toLowerCase();
+  if (!['fast', 'slow', 'price', '-price', 'date', 'repeat', '-repeat', 'answer', 'default'].includes(ordering)) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+  }
+
+  try {
+    client = await db.connect(req);
+    const order = await orderingDB.getOrder(client, ordering);
+    const lectures = await lectureDB.getLectures(client, categoryId, skillId, order);
+    let i = 1;
+    lectures[0].tags = [lectures[0].tagName];
+    delete lectures[0].tagName;
+
+    for (;;) {
+      if (lectures[i].id === lectures[i - 1].id) {
+        lectures[i - 1].tags.push(lectures[i].tagName);
+        lectures.splice(i, 1);
+      } else {
+        lectures[i].tags = [lectures[i].tagName];
+        delete lectures[i].tagName;
+        i++;
+      }
+      if (i === lectures.length) {
+        break;
+      }
     }
-    ordering = ordering.toLowerCase();
-    if (!(["fast", "slow", "price", "-price", "date", "repeat", "-repeat", "answer", "default"].includes(ordering))) {
-        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+
+    if (!order.column) {
+      lectures.sort((lecture) => -lecture.tags.length);
     }
 
-    try {
-        client = await db.connect(req);
-        const order = await orderingDB.getOrder(client, ordering);
-        const lectures = await lectureDB.getLectures(client, categoryId, skillId, order)
-        let i = 1;
-        lectures[0].tags = [lectures[0].tagName];
-        delete lectures[0].tagName;
-
-        while (true) {
-            if (lectures[i].id === lectures[i - 1].id) {
-                lectures[i - 1].tags.push(lectures[i].tagName);
-                lectures.splice(i, 1);
-            } else {
-                lectures[i].tags = [lectures[i].tagName];
-                delete lectures[i].tagName;
-                i++;
-            }
-            if (i === lectures.length) {
-                break;
-            }
-        }
-
-        if (!order.column) {
-            lectures.sort(lecture => -lecture.tags.length)
-        }
-
-        res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_LECTURES_SUCCESS, lectures));
-    } catch (error) {
-        functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-    } finally {
-        client.release();
-    }
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_LECTURES_SUCCESS, lectures));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
 };
