5c512e11ca3629f5823ea5f3025df6bfed69a6ab
=Jaeho-Jo <= jojaeho1230@naver.com>
2022-01-19 22:45:41 +0900

    [FEAT] Table_id 추가

--- functions/db/rank.js
+++ functions/db/rank.js
@@ -3,7 +3,7 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const getRankRequestLecture = async (client) => {
     const { rows } = await client.query(
         `
-        SELECT c.name , skill ,number FROM ranking
+        SELECT  ranking.id , c.name , skill ,number FROM ranking
         LEFT JOIN category c on c.id = ranking.category_id
         ORDER BY number DESC
         LIMIT 4;
